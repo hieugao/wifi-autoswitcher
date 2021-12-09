40dc666e600409cd7aeab836498c4917d2c8e5e5
hieugao <hieugaomoc@gmail.com>
2021-12-09 11:40:56 +0700

    🎉 Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# wifi-autoswitcher
-_Switch to another saved access point when the current network failed to reach the internet_
+# 🔄 Wifi Autoswitcher
+<p align="center">
+  <img with="192" height="192" src="https://cdn-icons-png.flaticon.com/512/4291/4291160.png" />
+</p>
+
+## Installation
+- [ ] Auto run
+- [ ] Etc
+
+## Credits
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
---
+++ network_switcher.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+gateway_ips='1.1.1.1 8.8.8.8'
+
+function check_gateways {
+    for ip in $gateway_ips; do
+        ping -c 3 $ip > /dev/null 2>&1
+        # The $? variable always contains the return code of the previous command.
+        # In BASH return code 0 usually means that everything executed successfully.
+        # In the next if we are checking if the ping command execution was successful.
+        if [[ $? == 0 ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+while sleep $(shuf -i 15-30 -n 1)s; do
+	if check_gateways; then
+	        echo "🟢 $(date +'%Y-%m-%d %T')"
+
+	else
+	        echo "🔴 $(date +'%Y-%m-%d %T')"
+		nmcli con up uuid $(nmcli con | sed '3,5!d' | shuf -n 1 | tr -s ' ' | cut -d ' ' -f 2)
+	fi
+done
