0bc095ee9530f8ea043d72d10bca291e71f9fa21
Hieu <hieugaomoc@gmail.com>
2021-12-10 18:09:32 +0700

    Remove hard coded the number of saved access points
    Instead get it automatically.

--- network_switcher.sh
+++ network_switcher.sh
@@ -2,6 +2,9 @@
 
 gateway_ips='1.1.1.1 8.8.8.8'
 
+# Subtract the using network and get from the next access point to it.
+max="$(($(nmcli con | grep wifi | wc -l ) - 1 + 2))"
+
 function check_gateways {
     for ip in $gateway_ips; do
         ping -c 3 $ip > /dev/null 2>&1
@@ -20,6 +23,8 @@ echo "🚀 Start at $(date +'%Y-%m-%d %T')"
 while sleep 25s; do
     if ! check_gateways; then
 	echo "🔴 $(date +'%Y-%m-%d %T')"
-	nmcli con up uuid $(nmcli con | sed '3,5!d' | shuf -n 1 | tr -s ' ' | cut -d ' ' -f 2)
+
+	# Source: https://stackoverflow.com/a/6022441/16553764
+	nmcli con up uuid $(nmcli con | sed -n "3p;${max}p" | shuf -n 1 | tr -s ' ' | cut -d ' ' -f 2)
     fi
 done
